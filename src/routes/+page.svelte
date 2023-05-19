f49e1842a97f3b6f3ee1cd9aed650481af0f5db0
engageintellect <jc9361@gmail.com>
2023-05-19 01:07:36 -0700

    cleaning up alternate fetch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
   import RecordCard from '$lib/component/RecordCard.svelte'
 
   async function records() {
-    const response = await fetch('/records')
+    const response = await fetch('/getRecords')
     res = await response.json()
     return res
   }
@@ -16,7 +16,13 @@
 
 <div>
   {#if $currentUser}
-    <div>Hello <strong>{$currentUser.name}!</strong></div>
+    <div class="text-4xl my-10">
+      Hello <strong>{$currentUser.name}!</strong>
+    </div>
+
+    <button class="btn btn-primary">
+      <a href="/addRecord">Add Record</a>
+    </button>
 
     {#if res !== undefined}
       <div class="mt-5 grid grid-cols-1 md:grid-cols-2 gap-5">
@@ -34,7 +40,7 @@
       </div>
     {/if}
   {:else}
-    <div>not logged in</div>
+    <div class="text-3xl my-10">Hey, stranger. You're not logged in.</div>
     <button class="btn btn-primary">
       <a href="/login">Login</a>
     </button>
