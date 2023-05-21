c88cd6640e00d38e6c6dbc490b167b2dda15cd7f
engageintellect <jc9361@gmail.com>
2023-05-21 00:50:44 -0700

    init testing

--- src/lib/component/RecordCard.svelte
+++ src/lib/component/RecordCard.svelte
@@ -1,40 +1,23 @@
 <script lang="ts">
-  import { pb } from '$lib/pocketbase'
-  import PocketBase from 'pocketbase'
+  let a = 0
+  let b = 0
+  let total = 0
 
-  export let name: string
-  export let artist: string
-  export let genre: string
-  export let year: string
-  export let id: string
+  async function add() {
+    const response = await fetch('../api/hi', {
+      method: 'POST',
+      body: JSON.stringify({ a, b }),
+      headers: {
+        'content-type': 'application/json',
+      },
+    })
 
-  const handleDelete = async (item_id: string) => {
-    await pb.collection('albums').delete(item_id)
-    location.reload()
+    total = await response.json()
   }
 </script>
 
-<div class="card card-side bg-base-100 shadow-xl">
-  <!-- <figure>
-    <img
-      src="https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg"
-      alt="Movie"
-    />
-  </figure> -->
-  <div class="card-body">
-    <h2 class="card-title text-3xl">{name}</h2>
+<input type="number" bind:value={a} /> +
+<input type="number" bind:value={b} /> =
+{total}
 
-    <div><strong>Artist:</strong> {artist}</div>
-    <div><strong>Genre:</strong> {genre}</div>
-    <div><strong>Year:</strong> {year}</div>
-
-    <div class="card-actions justify-end">
-      <button
-        on:click={() => {
-          handleDelete(id)
-        }}
-        class="p-3 btn btn-error">delete</button
-      >
-    </div>
-  </div>
-</div>
+<button on:click={add}>Calculate</button>
